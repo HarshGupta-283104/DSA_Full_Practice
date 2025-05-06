0187e77a803cdf0ac8783c23399df8bf9cd00943
harsh-gupta283104 <harsh.gupta332694@gmail.com>
2025-05-06 16:15:28 +0530

    I have used Scanner here

--- First.java
+++ First.java
@@ -1,5 +1,4 @@
-public class First {
-
+class First{
     public static void main(String[] args) {
         System.out.println("Radhe Radhe");
         System.out.println("Enter Your Lucky number: ");
