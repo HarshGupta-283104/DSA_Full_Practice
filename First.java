d1855b6c1233e234bf1107c3adf16182d01d09bf
harsh-gupta283104 <harsh.gupta332694@gmail.com>
2025-05-06 16:10:40 +0530

    I have used Scanner In the program

--- First.java
+++ First.java
@@ -1,5 +1,13 @@
+
+import java.util.*;
+
 class First{
     public static void main(String[] args) {
         System.out.println("Radhe Radhe");
+        System.out.println("Enter Your Lucky number: ");
+        Scanner sc = new Scanner(System.in);
+        int num_1 = sc.nextInt();
+        System.out.println(num_1);
+
     }
 }
