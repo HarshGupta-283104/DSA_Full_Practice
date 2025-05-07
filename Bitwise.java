95674a7f9702bd8a75cebec79ae58ad3a7306081
harsh-gupta283104 <harsh.gupta332694@gmail.com>
2025-05-07 13:57:52 +0530

    Complete Bitwise

--- Bitwise.java
+++ Bitwise.java
@@ -1,5 +1,23 @@
 public class Bitwise {
     public static void main(String[] args) {
         System.out.println("This program show Bitwise Operator. How they work or What is the work.");
+
+        int p = 9, q = 10;
+        System.out.println("OR");
+        System.out.println(p | q);
+        System.out.println("AND");
+        System.out.println(p & q);
+        System.out.println("XOR");
+        System.out.println(p ^ q);
+        System.out.println("Left Shift");
+        System.out.println(p << 1);
+
+        System.out.println(q << 1);
+        System.out.println("Right Shift");
+        System.out.println(q >> 2);
+        System.out.println(p >> 2);
+
+        
     }
+
 }
