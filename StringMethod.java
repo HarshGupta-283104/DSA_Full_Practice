d980cf24e5b68ac2731435f28f37b5afa08960a2
harsh-gupta283104 <harsh.gupta332694@gmail.com>
2025-05-08 12:14:56 +0530

    I have created and started learning String Method

---
+++ StringMethod.java
@@ -0,0 +1,7 @@
+public class StringMethod {
+    public static void main(String[] args) {
+        String name = "Harsh";
+        int lenght = name.length();
+        System.out.println(lenght);
+    }
+}
