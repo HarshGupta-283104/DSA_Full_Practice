8ad1737e448a85b2ed190e76d1f83a4c08cf5b98
harsh-gupta283104 <harsh.gupta332694@gmail.com>
2025-05-08 12:17:19 +0530

    I have created and started learning String Method

--- StringMethod.java
+++ StringMethod.java
@@ -1,7 +1,13 @@
 public class StringMethod {
     public static void main(String[] args) {
         String name = "Harsh";
-        int lenght = name.length();
-        System.out.println(lenght);
+        int length = name.length();
+        System.out.println(length);
+
+        System.out.println("Now I will check toLowerCase toUpperCase Method.");
+
+        System.out.println(name.toLowerCase());
+
+        System.out.println(name.toUpperCase());
     }
 }
